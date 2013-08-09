d12139a095697bed6deeb9e81110841a5058b889
furkan <furkanilgin@gmail.com>
2013-08-09 18:04:00 +0300

    logout function is added

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,8 +3,15 @@ class Home extends CI_Controller{
 
 	public function __construct(){
 		parent::__construct();
-		$this->load->helper('cookie');
-		//$this->load->library('session');
-		echo get_cookie('pass');
+		$this->load->helper("url");
+		$this->load->library('session');
+		if(!$this->session->userdata("valid_user")){
+			redirect("/login");
+		}
+	}
+	
+	public function logout(){
+		$this->session->unset_userdata('valid_user');
+		redirect("/login");
 	}
 }
