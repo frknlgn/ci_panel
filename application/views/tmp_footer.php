903a2551be4829d98d19a0856f26ad4a7ae8a7cf
furkan <furkanilgin@gmail.com>
2013-08-17 00:26:29 +0300

    header, footer templates and content file is added

---
+++ application/views/tmp_footer.php
@@ -0,0 +1,44 @@
+<div class="clear"></div>
+		</div>
+	<!--  end content-table-inner  -->
+	</td>
+	<td id="tbl-border-right"></td>
+</tr>
+<tr>
+	<th class="sized bottomleft"></th>
+	<td id="tbl-border-bottom">&nbsp;</td>
+	<th class="sized bottomright"></th>
+</tr>
+</table>
+
+
+<div class="clear">&nbsp;</div>
+
+</div>
+<!--  end content -->
+<div class="clear">&nbsp;</div>
+</div>
+<!--  end content-outer -->
+
+
+
+<div class="clear">&nbsp;</div>
+    
+<!-- start footer -->         
+<div id="footer">
+	<!--  start footer-left -->
+	<div id="footer-left">
+	TK Show Team Admin Panel</div>
+	<!--  end footer-left -->
+	<div class="clear">&nbsp;</div>
+</div>
+<!-- end footer -->
+</form>
+
+<script src="js/script.js"></script>
+<script type="text/javascript" src="ckeditor/ckeditor.js?t=B8DJ5M3"></script>
+<script type="text/javascript">//<![CDATA[
+CKEDITOR.replaceAll();
+//]]></script>
+</body>
+</html>
